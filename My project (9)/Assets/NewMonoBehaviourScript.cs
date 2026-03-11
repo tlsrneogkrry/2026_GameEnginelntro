c064e423840e03f497cf112b8cdfaeb14429f60e
tlsrneogkrrky <k01066530458@gmail.com>
2026-03-11 16:22:53 +0900

    2026137018이은상

---
+++ My project (9)/Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("¾È³ç ¼¼»ó¾Æ?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ My project (9)/Assets/NewMonoBehaviourScript1.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewMonoBehaviourScript1 : MonoBehaviour
+{
+    private Vector2 moveInput;
+    public float movespeed = 7f;
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveInput.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveInput.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        transform.Translate(Vector3.right *movespeed * moveInput.x * Time.deltaTime);
+    }
+}
