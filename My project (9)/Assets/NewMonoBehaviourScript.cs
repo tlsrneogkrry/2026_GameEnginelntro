e5a095d915bc444dc6d5ed43062ce15ee9e85d21
tlsrneogkrry <k01066530458@gmail.com>
2026-03-18 14:31:05 +0900

    캐릭터 움직임

--- My project (9)/Assets/NewMonoBehaviourScript.cs
+++ My project (9)/Assets/NewMonoBehaviourScript.cs
@@ -6,7 +6,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("¾È³ç ¼¼»ó¾Æ?");
+        //Debug.Log("¾È³ç ¼¼»ó¾Æ?");
     }
 
     // Update is called once per frame
--- My project (9)/Assets/NewMonoBehaviourScript1.cs
+++ My project (9)/Assets/NewMonoBehaviourScript1.cs
@@ -4,13 +4,28 @@ using UnityEngine.InputSystem;
 public class NewMonoBehaviourScript1 : MonoBehaviour
 {
     private Vector2 moveInput;
-    public float movespeed = 7f;
+    public float movespeed = 15f;
+    public float jumpForce = 10f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
     }
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)//점프 버튼을 누르면
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+    
 
-   
+
 
     // Update is called once per frame
     void Update()
